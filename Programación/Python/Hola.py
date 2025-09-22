a687f01915a4ff8e26ba1fe187cf1f8ce520f8bd
cm-04-mrls <cammorori@alu.edu.gva.es>
2025-09-22 11:17:57 +0200

    Delete Programación/Python directory

--- Programación/Python/Hola.py
+++
@@ -1,6 +0,0 @@
-#Hola esto es una linea de python 
-print("Hola mundo")
-
-numero = 3
-texto = "Esto es un texto"
-resultado  = True 
--- Programación/Python/Suma.py
+++
@@ -1,8 +0,0 @@
-print("dame un número")
-número1 = int(input())
-print("dame otro")
-número2 = int(input())
-suma=número1+número2
-texto = "La suma de los dos números" 
-print(texto , suma)
-print(f"La suma de los dos números es {suma}")
